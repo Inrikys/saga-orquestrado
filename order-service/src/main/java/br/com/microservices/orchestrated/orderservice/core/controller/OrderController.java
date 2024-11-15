4392c421338a62027f6bee4271443e5ea6400b62
Henrique Sousa <henrique661@gmail.com>
2024-11-15 18:13:03 -0300

    :sparkles: order controller

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/OrderController.java
@@ -0,0 +1,25 @@
+package br.com.microservices.orchestrated.orderservice.core.controller;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Order;
+import br.com.microservices.orchestrated.orderservice.core.dto.OrderRequest;
+import br.com.microservices.orchestrated.orderservice.core.service.OrderService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.createOrder(orderRequest);
+    }
+}
